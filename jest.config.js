72f62891634d8fb776f093d4e29d16c2b5604e9b
clark-lindsay <clarklindsay96@gmail.com>
2020-09-23 19:12:55 -0400

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "transform": {
+      "^.+\.svelte$": [
+        "svelte-jester",
+        {
+          "preprocess": true
+        }
+      ],
+      "^.+\.ts$": "ts-jest"
+    },
+    "moduleFileExtensions": [
+      "js",
+      "ts",
+      "svelte"
+    ],
+    "setupFilesAfterEnv": [
+			"@testing-library/jest-dom/extend-expect",
+      "./jest.setup.ts"
+    ]
+};
---
+++ svelte.config.js
@@ -0,0 +1,7 @@
+const sveltePreprocess = require('svelte-preprocess');
+
+module.exports = {
+  preprocess: sveltePreprocess({
+    // any options go here, see svelte-preprocess docs for details
+  }),
+};
